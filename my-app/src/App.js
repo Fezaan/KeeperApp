627ea804583b3829f14be3da275dead25eab6ee3
Fezaan <110244796+Fezaan@users.noreply.github.com>
2023-06-02 19:00:00 +0530

    Props added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,12 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import Note from "./components/note";
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <>
       <Header />
-      <Note />
+      <Note title="Fezaan" content="Is a noob" />
       <Footer></Footer>
     </>
-  )
+  );
 }
